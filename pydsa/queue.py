fbd619e40395b5bf5e683cde1f83ba20a2f1ba79
bmcculley <bmcculley@ejank.com>
2022-10-19 16:11:08 -0400

    Make the Queue FIFO

--- pydsa/queue.py
+++ pydsa/queue.py
@@ -9,10 +9,10 @@ class Queue:
         self.queue.insert(value, 0)
 
     def dequeue(self):
-        return self.queue.pop_back()
+        return self.queue.pop_front()
 
     def peek(self):
-        return self.queue.peek_back()
+        return self.queue.peek_front()
 
     def is_empty(self):
         return self.queue.is_empty()
